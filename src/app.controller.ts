1fe42ff2462935722ea04e4f273a7d1288756c95
endyd9 <endyd9@gmail.com>
2023-08-19 02:47:09 +0900

    파라미터 없이 지정 일자 자동수집으로 바꿈

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -34,66 +34,89 @@ interface RequestQuery {
   year: string;
 }
 
+const codeList: number[] = [
+  11680, 11740, 11305, 11500, 11620, 11215, 11530, 11545, 11350, 11320, 11230,
+  11590, 11440, 11410, 11650, 11200, 11290, 11710, 11470, 11560, 11170, 11380,
+  11010, 11140, 11260,
+];
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('/')
-  async saveApt(@Query() query: RequestQuery) {
-    let month = '1';
-    for (let i = 0; i < 12; i++) {
-      const {
-        data: {
-          response: {
-            body: {
-              items: { item },
+  async saveApt() {
+    // for (let year = 2016; year < 2023; year++) {
+    const year = 2016;
+    for (const code of codeList) {
+      let month = '1';
+      for (let i = 0; i < 12; i++) {
+        const {
+          data: {
+            response: {
+              body: {
+                items: { item },
+              },
             },
           },
-        },
-      } = await axios.get<AptResponse>(
-        `${process.env.REQUEST_URL}?serviceKey=${
-          process.env.API_KEY
-        }&pageNo=${1}&numOfRows=${1000}&LAWD_CD=${query.code}&DEAL_YMD=${
-          query.year + month.padStart(2, '0')
-        }`,
-      );
+        } = await axios.get<AptResponse>(
+          `${process.env.REQUEST_URL}?serviceKey=${
+            process.env.API_KEY
+          }&pageNo=${1}&numOfRows=${1000}&LAWD_CD=${code}&DEAL_YMD=${
+            year + month.padStart(2, '0')
+          }`,
+        );
 
-      if (item !== undefined) {
-        if (item.length === undefined) {
-          const data: Prisma.AptCreateInput = {
-            name: item.아파트,
-            dedicatedArea: item.전용면적,
-            floor: item.층,
-            buildYear: item.건축년도,
-            treadDate: +(item.년 + '' + item.월 + '' + item.일 + ''),
-            treadAmount: +item.거래금액.replaceAll(',', ''),
-            cityCode: item.지역코드,
-            dong: item.법정동,
-            roadName: item.도로명,
-            buildingNum: item.도로명건물본번호코드,
-            buildingMinorNum: item.도로명건물부번호코드,
-          };
-          await this.appService.saveApt(data);
-        } else {
-          item.forEach(async (apt: AptInfo) => {
+        if (item !== undefined) {
+          if (item.length === undefined) {
             const data: Prisma.AptCreateInput = {
-              name: apt.아파트,
-              dedicatedArea: apt.전용면적,
-              floor: apt.층,
-              buildYear: apt.건축년도,
-              treadDate: +(apt.년 + '' + apt.월 + '' + apt.일 + ''),
-              treadAmount: +apt.거래금액.replaceAll(',', ''),
-              cityCode: apt.지역코드,
-              dong: apt.법정동,
-              roadName: apt.도로명,
-              buildingNum: apt.도로명건물본번호코드,
-              buildingMinorNum: apt.도로명건물부번호코드,
+              name: item.아파트,
+              dedicatedArea: item.전용면적,
+              floor: item.층,
+              buildYear: item.건축년도,
+              treadDate:
+                item.년 +
+                '' +
+                item.월 +
+                ''.padStart(2, '0') +
+                item.일 +
+                ''.padStart(2, '0'),
+              treadAmount: +item.거래금액.replaceAll(',', ''),
+              cityCode: item.지역코드,
+              dong: item.법정동,
+              roadName: item.도로명,
+              buildingNum: item.도로명건물본번호코드,
+              buildingMinorNum: item.도로명건물부번호코드,
             };
             await this.appService.saveApt(data);
-          });
+          } else {
+            item.forEach(async (apt: AptInfo) => {
+              const data: Prisma.AptCreateInput = {
+                name: apt.아파트,
+                dedicatedArea: apt.전용면적,
+                floor: apt.층,
+                buildYear: apt.건축년도,
+                treadDate:
+                  apt.년 +
+                  '' +
+                  apt.월 +
+                  ''.padStart(2, '0') +
+                  apt.일 +
+                  ''.padStart(2, '0'),
+                treadAmount: +apt.거래금액.replaceAll(',', ''),
+                cityCode: apt.지역코드,
+                dong: apt.법정동,
+                roadName: apt.도로명,
+                buildingNum: apt.도로명건물본번호코드,
+                buildingMinorNum: apt.도로명건물부번호코드,
+              };
+              await this.appService.saveApt(data);
+            });
+          }
         }
+        month = +month + 1 + '';
       }
-      month = +month + 1 + '';
     }
+    // }
   }
 }
